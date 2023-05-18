9652b514ce0a14a1f07651b4e88f7f4abf104c0f
= <18155469483@163.com>
2023-05-18 19:38:07 +0800

    redis

--- java_redis/src/main/java/redis/Ping.java
+++ java_redis/src/main/java/redis/Ping.java
@@ -9,26 +9,40 @@ import java.io.IOException;
 
 public class Ping {
     public static void main(String[] args) {
-        //Jedis jedis =new Jedis("127.0.0.1", 6379);
-        Jedis jedis =new Jedis("192.168.42.111", 6379);
-        String pingResult = jedis.ping();
-        if("PONG".equals(pingResult)){
-            System.out.println("连接成功");
+        Jedis jedis = null;
+        try {
+            //Jedis jedis =new Jedis("127.0.0.1", 6379);
+            jedis = new Jedis("192.168.42.111", 6379);
+            String pingResult = jedis.ping();
+            if ("PONG".equals(pingResult)) {
+                System.out.println("连接成功");
+            }
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
         }
+
     }
 
+
     @Test
-    public static void test() throws IOException {
-        File file = new File("F:\test.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        fileOutputStream.write("*3\r\n".getBytes("utf-8"));
-        fileOutputStream.write("$3\r\n".getBytes("utf-8"));
-        fileOutputStream.write("set\r\n".getBytes("utf-8"));
-        fileOutputStream.write("$5\r\n".getBytes("utf-8"));
-        fileOutputStream.write("hello\r\n".getBytes("utf-8"));
-        fileOutputStream.write("$5\r\n".getBytes("utf-8"));
-        fileOutputStream.write("world\r\n".getBytes("utf-8"));
-        fileOutputStream.flush();
-        fileOutputStream.close();
+    public static void test() {
+        try {
+            File file = new File("F:\test.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write("*3\r\n".getBytes("utf-8"));
+            fileOutputStream.write("$3\r\n".getBytes("utf-8"));
+            fileOutputStream.write("set\r\n".getBytes("utf-8"));
+            fileOutputStream.write("$5\r\n".getBytes("utf-8"));
+            fileOutputStream.write("hello\r\n".getBytes("utf-8"));
+            fileOutputStream.write("$5\r\n".getBytes("utf-8"));
+            fileOutputStream.write("world\r\n".getBytes("utf-8"));
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
